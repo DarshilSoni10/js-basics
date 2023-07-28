45f5bb18da15461d8e56af28a7e8aaf30487dd7c
DarshilSoni10 <darshilsoni10@gmail.com>
2023-07-28 10:25:49 +0530

    Learn 1.0

---
+++ functions.js
@@ -0,0 +1,92 @@
+// Solution 1
+
+function addNumbers(a, b) {
+  return a * b;
+}
+
+// Calling the function and storing the result in a variable
+let sum = addNumbers(5, 10);
+console.log(sum); // Output: 50
+
+// Solution 2
+
+const name = "Darshil Soni";
+function toUpper(text) {
+  const upperCased = text.toUpperCase();
+  console.log(upperCased);
+}
+
+toUpper(name);
+
+// Solution 3
+
+function findLargerNumber(a, b) {
+  if (a > b){ 
+    return a;
+  } else { 
+    return b;
+  }
+}
+
+let solution1 = findLargerNumber(10, 5);
+console.log(solution1);
+
+let solution2 = findLargerNumber(21, 20);
+console.log(solution2);
+
+// Solution 4
+
+function checkEvenOrOdd(number) {
+  if (number % 2 === 0) {
+    return `The number ${number} is even`;
+  } else {
+    return `The number ${number} is odd` ;
+  }
+}
+
+let result1 = checkEvenOrOdd(99999);
+console.log(result1);
+
+let result2 = checkEvenOrOdd(1234567890);
+console.log(result2);
+
+// here are some of the examples of Boolean
+
+// Solution 1: Check if a number is positive or negative
+
+function isTrue(number) {
+  return number < 220;
+}
+
+console.log(isTrue(5)); // Output: true
+console.log(isTrue(1200)); // Output: false
+console.log(isTrue(-1200)); // Output: true
+
+// Solution 2: Determine if a number is even or odd
+
+function isOdd(number) {
+  return number % 2 === 0;
+}
+
+console.log(isOdd(1200)); // Output: true
+console.log(isOdd(1299)); // Output: false
+
+// Solution 3: define a boolean function where it says that CR7 is the goat is true and Messi is the goat is false
+
+function isGOAT(statement) {
+  return statement === "CR7 is the Goat";
+}
+
+console.log(isGOAT("CR7 is the Goat")); // Output: true
+console.log(isGOAT("Messi is the Goat")); // Output: false
+console.log(isGOAT("CR7 is not the Goat")); // Output: false
+
+// Solution 4: Check if a number is a multiple of another number
+
+function isMultipleOf(number, multiple) {
+  return number % multiple === 0;
+}
+
+console.log(isMultipleOf(10, 2)); // Output: true
+console.log(isMultipleOf(20, 4)); // Output: true
+console.log(isMultipleOf(100, 3)); // Output: false
---
+++ strings.js
@@ -0,0 +1,142 @@
+console.log("Hello my name is " + "DS"); // Output: Hello my name is DS
+
+let str1 = 'Hello, World!';
+let str2 = "JavaScript is awesome!";
+let str3 = '12345'; // This is also a string, not a number
+
+console.log(str1, str2); //Output: Hello, World! JavaScript is awesome!
+
+let firstname = "Darshil";
+let middlename = "Jigneshbhai";
+let lastname = "Soni";
+let fullname = firstname + " " + middlename + " " + lastname;
+
+console.log(fullname); // Output: Darshil Jigneshbhai Soni
+
+// Solution 1: Getting the length of a string
+
+let message = "Oppenheimer!";
+let messageLength = message.length;
+console.log(messageLength); // Output: 12
+
+// Solution 2: Converting a string to Uppercase or Lowercase
+
+let text = "Batman: Dark Knight Joker is the best villain ever!"
+let upperCaseText = text.toUpperCase();
+let lowerCaseText = text.toLowerCase();
+
+console.log(upperCaseText);
+console.log(lowerCaseText);
+
+// Soultion 3: 
+
+// Concatenation example
+
+const Name = "Darshil Soni";
+const age = 25;
+
+console.log(
+    "Hello it's me " + Name + " and my age is " + age + " and I will be pro at this. "
+);
+
+console.log(`Hello it's me ${Name} and my age is ${age} and this is an example of Concatenation`);
+
+// If Else Statement
+
+const Age = 17;
+
+if (Age >= 18) {
+    console.log("You are Adult");
+} else {
+    console.log("You are Minor");
+}
+
+// Solution 1: Determine if a number is Positive or Negative
+
+function checkNumberSign(number) {
+    if (number > 0) {
+        console.log("The number is Positive.");
+    } else if (number < 0) {
+        console.log("The number is Negative.");
+    } else {
+        console.log("The Number is Zero.");
+    }
+}
+
+checkNumberSign(10); // The output should be 'Positive' as given input value i,e., 10
+checkNumberSign(-10); // The output should be 'Negative'. As -ve sign indicates negative numbers in mathematics.
+checkNumberSign(0); // The output should be 'Zero'.
+
+// Solution 2: Check if a person is eligible for voting
+
+function checkVotingEligibilty(age) {
+    if (age >= 18){
+        console.log("You are eligible for voting.")
+    } else {
+        console.log("Sorry! You cannot vote yet!")
+    }
+}
+
+checkVotingEligibilty(10); // Output: Sorry! You cannot vote yet!
+checkVotingEligibilty(22); // Output: You are eligible for voting!
+
+// Solution 3: Check if a person's username and password are correct
+
+function authenticateUser(username, password) {
+    const validUsername = "darshil1";
+    const validPassword = "pass123";
+    
+    if(username === validUsername && password === validPassword) {
+        console.log("Authentication successful, Welcome! " + username + "!");
+    } else {
+        console.log("Authentication failed! Invalid Username or password combination.");
+    }
+}
+
+authenticateUser("user1", "pass123"); // Output: Authentication Failed! Invalid Username or password combination. 
+authenticateUser("darshil1", "pass123");// Output: Authentication Successful - welcome darshil1 !
+
+// Test of all the things learned 24/07 (variables, Booleans, functions, strings, concatenation, if else elements)
+
+// 1:  Determine if a person is eligible for a discount based on their age.
+
+function checkDiscountEligibility(age) {
+    const minimumAge = 21;
+    const isAdult = false;
+    if (age >= minimumAge || isAdult) {
+        return "You are eligible to Drink Alcohol.";
+    } else {
+        return `Unfortunately you have not reached the required Age of ${minimumAge}.`;
+    }
+}
+
+let customerAge = 25;
+console.log(checkDiscountEligibility(customerAge));
+// Output: You are eligible to Drink Alcohol.
+
+let minorAge = 15;
+console.log(checkDiscountEligibility(minorAge));
+// Output: Unfortunately you have not reached the required Age of 21.
+
+// 2: Generate a welcome message based on a user's name and language preference.
+
+function generateWelcomeMessage(name, language) {
+    let greetingText = "";
+    if (language === "english") {
+        greetingText = "Hello, " + name + "!";
+    } else if (language === "spanish"){
+        greetingText = "¡Hola, " + name +"!";
+    } else {
+        greetingText = "Welcome, " + name + "!";
+    }
+
+    return greetingText;
+}
+
+let userName = "Darshil";
+let userLanguage = "english";
+console.log(generateWelcomeMessage(userName, userLanguage));
+
+let userName2 = "Niyati";
+let userLanguage2 = "spanish";
+console.log(generateWelcomeMessage(userName2, userLanguage2));
